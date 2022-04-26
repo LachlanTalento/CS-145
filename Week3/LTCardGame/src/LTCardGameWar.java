b4f1491fcc6796451dac78ed8c9be981f89fb489
Lachlan.T <talentolachlan@gmail.com>
2022-04-26 10:02:24 -0700

    Created LTCardGame

---
+++ Week3/LTCardGame/src/LTCardGameWar.java
@@ -0,0 +1,10 @@
+// Programmer: Lachlan Talento
+// Date:	   4/26/2022
+// Course:     CS&145
+// Lab:        Card Game
+//
+// This program will do the following: 
+
+public class LTCardGameWar {
+	
+}
