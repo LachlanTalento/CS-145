bf3fff3b8bab7e9c2d1353d1eea59c9198c3a885
PorkeyYT <62969602+PorkeyYT@users.noreply.github.com>
2022-05-31 22:36:51 -0700

    Update LTWordSearchGenerator.java

--- Week7/Word Search Generator/src/LTWordSearchGenerator.java
+++ Week7/Word Search Generator/src/LTWordSearchGenerator.java
@@ -1,3 +1,10 @@
+// Programmer: Lachlan Talento
+// Date:       5/2/2022
+// Course:     CS&145
+// Lab:        Towers of Hanoi
+//
+// This program will do the following: Generate a crossword puzzle
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
